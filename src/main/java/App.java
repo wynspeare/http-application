278d8713b4f047ac06b74232580bb5068702ee7d
wynspeare <caroline@8thlight.com>
2019-09-25 13:02:11 -0500

    Added server dependency and App can build a new server with custom routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,39 @@
+import server.HTTPServer;
+import server.Router;
+import server.handlers.DefaultHandler;
+import server.handlers.EchoHandler;
+import server.handlers.RedirectHandler;
+
+import static server.HTTPServer.serverLogger;
+
 public class App {
+  private static HTTPServer httpServer;
 
   public static void main(String[] args) {
-    System.out.println("Hello from Application");
+    httpServer = new HTTPServer();
+    start();
   }
+
+  public static void start() {
+    Router router = createRouter();
+    try {
+      httpServer.serve(router);
+    } finally {
+      httpServer.close();
+    }
+  }
+
+  public static Router createRouter() {
+    Router router = new Router(serverLogger);
+
+    router.addRoute("GET", "/simple_get", new DefaultHandler());
+    router.addRoute("HEAD", "/simple_get", new DefaultHandler());
+    router.addRoute("HEAD", "/get_with_body", new DefaultHandler());
+    router.addRoute("POST", "/echo_body", new EchoHandler());
+    router.addRoute("GET", "/redirect", new RedirectHandler("http://127.0.0.1:5000/simple_get"));
+    router.addRoute("GET", "/test", new DefaultHandler());
+
+    return router;
+  }
+
 }
