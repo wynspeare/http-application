47b8504bfbf3f44f67f4ab96109b476419699573
wynspeare <caroline@8thlight.com>
2019-09-26 15:12:37 -0500

    ServerLogger instantiated in application not server

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,17 @@ import server.Router;
 import server.handlers.DefaultHandler;
 import server.handlers.EchoHandler;
 import server.handlers.RedirectHandler;
+import server.logger.ServerLogger;
 
-import static server.HTTPServer.serverLogger;
+//import static server.HTTPServer.serverLogger;
 
 public class App {
   private static int defaultPort = 5000;
+  public static final ServerLogger serverLogger = createLogger("Logs");
+
+  public static ServerLogger createLogger(String directory) {
+    return new ServerLogger(directory);
+  }
 
   public static void main(String[] args) {
     HTTPServer httpServer = new HTTPServer();
